16e85c93dd9d4841c45dc22fa416ac6180096779
wenbo <wenbochen@1997.gmail.com>
2024-08-07 11:51:39 -0400

    add new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp  from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
 
   return (
-    <>
-      <p className='text-red-50'>hello111111</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+  
+      </Routes>
+    </BrowserRouter>
   )
 }
 
