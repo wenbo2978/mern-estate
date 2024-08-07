25e6013e12b79ae55272151320a6868d5c7e7975
wenbo <wenbochen@1997.gmail.com>
2024-08-07 16:13:45 -0400

    connent to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
+//console.log("Mongo:"+process.env.MONGO_URL);
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+  console.log('connect to MongoDB');
+}).catch((err)=>{
+  console.log('err: ' + err);
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!!!');
