cdca297136e06222bdd1bec2efdf1681db1c43cd
wenbo <wenbochen@1997.gmail.com>
2024-08-08 22:45:33 -0400

    update the header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp  from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import Headers from './components/Headers'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
 
@@ -16,7 +17,10 @@ function App() {
         <Route index element={<Home/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
         <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/profile' element={<Profile/>}/>
+        <Route element={<PrivateRoute/>}>
+          <Route path='/profile' element={<Profile/>}/>
+        </Route>
+        
         <Route path='/about' element={<About/>}/>
   
       </Routes>
--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Headers = () => {
+  const {currentUser} = useSelector(state => state.user);
   return (
     <div className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -25,9 +27,22 @@ const Headers = () => {
           <Link to={'/about'}>
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
-          <Link to={'/sign-in'}>
-            <li className='text-slate-700 hover:underline'>Sign in</li>
-          </Link>
+          {
+            currentUser ? (
+              <Link to={'/profile'}>
+                <img 
+                  className='rounded-full h-7 w-7 object-cover'
+                  src={currentUser.avatar} 
+                  alt='profile'
+                />
+              </Link>
+            ) : (
+              <Link to={'/sign-in'}>
+                <li className='text-slate-700 hover:underline'>Sign in</li>
+              </Link>
+            )
+          }
+          
           
         </ul>
       </div>
