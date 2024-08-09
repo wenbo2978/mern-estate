6e9b40c02cab16741697adad2a7b1f2e69cb4fc3
wenbo <wenbochen@1997.gmail.com>
2024-08-09 17:51:37 -0400

    create listing api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import userRouter from './routes/user-route.js';
 import authRouter from './routes/auth-route.js';
+import listingRouter from './routes/listing-route.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
@@ -29,6 +30,7 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 //Error-Handling Middleware
 app.use((err, req, res, next) => {
