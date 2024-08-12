3fef0228ae589a917660f7448076960219d3f2fd
wenbo <wenbochen@1997.gmail.com>
2024-08-12 11:49:59 -0400

    complete listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,8 @@ import {Swiper, SwiperSlide} from 'swiper/react';
 import SwiperCore from 'swiper';
 import {Navigation} from 'swiper/modules';
 import 'swiper/css/bundle';
+import {FaBath, FaBed, FaChair, FaParking} from 'react-icons/fa'
+
 
 const Listing = () => {
   SwiperCore.use(Navigation);
@@ -13,8 +15,9 @@ const Listing = () => {
   const [error, setError] = useState(false);
   useEffect(()=>{
     const fetchList = async ()=>{
+      console.log('start');
       try{
-        setLoading(true);
+        //setLoading(true);
         setError(false);
         const listingId = params.id;
         const res = await fetch(`/api/listing/getListing/${listingId}`);
@@ -26,6 +29,7 @@ const Listing = () => {
         }
         setListingData(data);
         setLoading(false);
+        console.log(data);
       }catch(err){
         setError(err);
         setLoading(false);
@@ -43,10 +47,10 @@ const Listing = () => {
       }
       {
         listingData && !error && !loading &&
-        <>
+        <div>
           <Swiper navigation>
             {
-              listingData?.imageUrls.map((url)=>(
+              listingData.imageUrls && listingData.imageUrls.map((url, index)=>(
                 <SwiperSlide key={url}>
                   <div className='h-[550px]' style={{
                     background: `url(${url}) center no-repeat`,
@@ -58,7 +62,68 @@ const Listing = () => {
               ))
             }
           </Swiper>
-        </>
+          <div className='max-w-4xl mx-auto p-5 gap-3 flex flex-col'>
+            <p className='font-semibold text-2xl'>
+              {
+                listingData.name
+              }
+              -$
+              {
+                listingData.offer ? 
+                listingData.discountPrice?.toLocaleString('en-US') : 
+                listingData.regularPrice?.toLocaleString('en-US')
+              }
+              {
+                listingData.type === 'rent' && ' / month'
+              }
+            </p>
+            <a 
+              className='mt-8 flex my-3 gap-1 font-semibold underline text-xl ' target='_blank' 
+              href={'https://maps.google.com/?q='+listingData.address}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+              </svg>
+              {listingData.address}
+            </a>
+            <div className='flex gap-4'>
+              <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 rounded-md m-2'>
+                {
+                  listingData.type === 'rent' ? 'For Rent' : 'For Sale'
+                }
+              </p>
+              {
+                listingData && listingData.offer && (
+                  <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md m-2'>
+                    ${+listingData.regularPrice - +listingData.discountPrice} discount
+                  </p>
+                )
+              }
+            </div>
+            <p className='text-slate-800 mt-2'>
+              <span className='font-semibold text-black'>Description - {' '}</span>
+              {listingData.description}
+            </p>
+            <ul className='text-green-900 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6 mt-5'>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaBed className='text-lg'/>
+                {listingData.bedrooms > 1 ? `${listingData.bedrooms} beds` : `${listingData.bedrooms} bed`}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaBath className='text-lg'/>
+                {listingData.bathrooms > 1 ? `${listingData.bathrooms} baths` : `${listingData.bathrooms} bath`}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaParking className='text-lg'/>
+                {listingData.parking ? 'Parking spot' : 'No parking'}
+              </li>
+              <li className='flex items-center gap-1 whitespace-nowrap '>
+                <FaChair className='text-lg'/>
+                {listingData.furnished ? 'Furnished' : 'Unfurnished'}
+              </li>
+            </ul>
+          </div>
+        </div>
         
 
       }
